6e29e5401f7cf2b1076a6a6424f8f8491701fd6e
Suman Shaw <78732037+Shaw145@users.noreply.github.com>
2025-03-23 23:41:54 +0530

    Update ForgotPassword.jsx

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -13,7 +13,7 @@ const ForgotPassword = () => {
     setIsLoading(true); // Start loading
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/forgot-password", {
+      const response = await fetch("https://shiftly-backend.onrender.com/api/auth/forgot-password", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
