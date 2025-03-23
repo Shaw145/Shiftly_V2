581b73e2e9692da0d86fb48d3054492c23591b42
Suman Shaw <78732037+Shaw145@users.noreply.github.com>
2025-03-23 23:40:58 +0530

    Update ResetPassword.jsx

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -15,7 +15,7 @@ const ResetPassword = () => {
     setIsLoading(true); // Start loading
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/reset-password", {
+      const response = await fetch("https://shiftly-backend.onrender.com/api/auth/reset-password", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
